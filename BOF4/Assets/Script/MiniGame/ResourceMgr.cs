ef3092c7f41a85f65ffa1cc4c3d1dfbef410bd8e
wencong_kaixia <wencong4996@gmail.com>
2015-09-19 11:23:07 +0800

    没dll依赖库，瞎改ResourceMgr

---
+++ BOF4/Assets/Script/MiniGame/ResourceMgr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class ResourceMgr : Singleton<ResourceMgr> {
+
+    public T Load<T>(string path) where T : UnityEngine.Object {
+        return Resources.Load<T>(path);
+    }
+
+    public void UnLoad<T>(T obj) where T : UnityEngine.Object {
+        Resources.UnLoadAsset(obj);
+    }
+}
+
