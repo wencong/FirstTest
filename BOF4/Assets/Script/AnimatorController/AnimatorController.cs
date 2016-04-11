e4ca715750a23fbd3aa6732ddfd29babbee50ed0
文聪_Mac <wencong4996@gmail.com>
2016-04-11 14:59:33 +0800

    增加AnimatorController

---
+++ BOF4/Assets/Script/AnimatorController/AnimatorController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorController : MonoBehaviour {
+	private Animator m_animator;
+	// Use this for initialization
+	void Start () {
+		m_animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Attack() {
+		m_animator.SetTrigger("attack");
+	}
+}
---
+++ BOF4/Assets/Script/GameBit/GameBitGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class GameBitGroup{
+	public const int m_nMacCount = 12;
+
+	public GameBitGroup () {
+	}
+
+
+}
+
+
